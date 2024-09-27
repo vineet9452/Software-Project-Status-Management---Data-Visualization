db6c0e4d16f6681f2bca28b39a6e14805073709d
vineet mishra <158786874+vineet9452@users.noreply.github.com>
2024-09-27 10:57:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
